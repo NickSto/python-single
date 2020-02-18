e58d94ea326ca1f5b6dbbfa83d976dc702cdbd4c
Nick <nmapsy@gmail.com>
2020-02-18 18:26:09 -0500

    dispatcher.py: Add parameters, with first one #?when for scheduling execution. - Use cron syntax for specifying when to execute a command. - Add "echo" command for debug. - Organize code a little better.

--- dispatcher.py
+++ dispatcher.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
 import argparse
+import collections
+import datetime
 import logging
 import pathlib
 import sys
 assert sys.version_info.major >= 3, 'Python 3 required'
 
+PERIODS = collections.OrderedDict(
+  (
+    ('min', {'dt':lambda dt: dt.minute}),
+    ('hr',  {'dt':lambda dt: dt.hour}),
+    ('dom', {'dt':lambda dt: dt.day}),
+    ('mon', {'dt':lambda dt: dt.month}),
+    ('week',{'dt':lambda dt: dt.weekday()+1})
+  )
+)
+
 DESCRIPTION = """Take actions based on the content of a simple input file."""
 
-
+#TODO: Add path whitelist.
 def make_argparser():
   parser = argparse.ArgumentParser(add_help=False, description=DESCRIPTION)
   options = parser.add_argument_group('Options')
   options.add_argument('infile', type=argparse.FileType('r'), default=sys.stdin, nargs='?',
     help='Input file. Omit to read from stdin.')
+  options.add_argument('-p', '--precision', type=int,
+    help='Time precision of execution. How many minutes since the last time this was executed? '
+      'Required for any command with a #?when parameter. Currently only applied to the #?when hour '
+      'and minute.')
   options.add_argument('-h', '--help', action='help',
     help='Print this argument help text and exit.')
   logs = parser.add_argument_group('Logging')
@@ -34,14 +50,16 @@ def main(argv):
   logging.basicConfig(stream=args.log, level=args.volume, format='%(message)s')
 
   for lines in chunk_input(args.infile):
-    command, args = parse_command(lines[0])
-    content = lines[1:]
     try:
-      fxn = COMMANDS[command]
-    except KeyError:
-      logging.error(f'Error: Unrecognized command {command!r} in line {lines[0]!r}')
-    else:
-      fxn(args, content)
+      command, chunk_args, params, content = parse_chunk(lines)
+    except ValueError as error:
+      logging.error(error)
+      continue
+    if 'when' in params:
+      if not execute_now(params['when'], args.precision):
+        continue
+    fxn = COMMANDS[command]
+    fxn(chunk_args, content)
 
 
 def chunk_input(lines):
@@ -57,12 +75,102 @@ def chunk_input(lines):
     yield chunk_lines
 
 
+def parse_chunk(chunk_lines):
+  if len(chunk_lines) <= 0:
+    raise ValueError('Received a chunk with no lines.')
+  first_line = chunk_lines[0]
+  command, args = parse_command(first_line)
+  content = []
+  params = {}
+  for line in chunk_lines[1:]:
+    if not content and line.startswith('#?'):
+      param_type, param = parse_params(line)
+      params[param_type] = param
+    else:
+      content.append(line)
+  return command, args, params, content
+
+
 def parse_command(command_line):
   assert command_line.startswith('#!'), command_line
   fields = command_line[2:].split()
   command = fields[0]
   args = fields[1:]
+  if command not in COMMANDS:
+    raise ValueError(f'Unrecognized command {command!r} in line {command_line!r}')
   return command, args
+
+
+def parse_params(params_line):
+  assert params_line.startswith('#?'), params_line
+  fields = params_line[2:].split()
+  param_type = fields[0]
+  param_args = fields[1:]
+  if param_type == 'when':
+    return 'when', parse_when_param(param_args)
+  else:
+    raise ValueError(f'Invalid parameter type {param_type!r}')
+
+
+def parse_when_param(args):
+  if len(args) != len(PERIODS):
+    raise ValueError(
+      f'Wrong number of arguments to when parameter (saw {len(args)}, need {len(PERIODS)}).'
+    )
+  time_spec = {}
+  for period, arg in zip(PERIODS, args):
+    if arg == '*':
+      value = None
+    else:
+      try:
+        value = int(arg)
+      except ValueError as error:
+        error.args = (f'Invalid when parameter {arg!r} (not an integer or *)',)
+        raise error
+    time_spec[period] = value
+  return time_spec
+
+
+def execute_now(time_spec, precision):
+  if precision is None:
+    logging.error('Error: Encountered #?when parameter, but no --precision given.')
+    return None
+  now = datetime.datetime.now()
+  # If the time_spec includes a day of any kind, are we on the right day?
+  for period, spec_value in time_spec.items():
+    if period in ('min', 'hr'):
+      continue
+    elif spec_value is None:
+      continue
+    dt_converter = PERIODS[period]['dt']
+    current_value = dt_converter(now)
+    if current_value != spec_value:
+      return False
+  # How many minutes after the time_spec are we executing?
+  spec_hr = time_spec['hr']
+  if spec_hr is None:
+    spec_hr = now.hour
+  spec_min = time_spec['min']
+  if spec_min is None:
+    spec_min = now.minute
+  spec_minutes = spec_hr*60 + spec_min
+  now_minutes = now.hour*60 + now.minute
+  diff = now_minutes - spec_minutes
+  if diff < 0:
+    diff += 24*60
+  logging.debug(f'Debug: Got a time diff of {diff} from {now_minutes} - {spec_minutes}')
+  if diff < 0:
+    raise ValueError(
+      f'time_spec seems to be > 24 hrs after now? (time_spec: {time_spec!r}, now: {now!r})'
+    )
+  if diff > precision:
+    return False
+  else:
+    return True
+
+
+def do_echo(args, content):
+  print(*args)
 
 
 def do_cat(args, content):
@@ -79,6 +187,7 @@ def do_cat(args, content):
 
 
 COMMANDS = {
+  'echo':do_echo,
   'cat':do_cat,
 }
 

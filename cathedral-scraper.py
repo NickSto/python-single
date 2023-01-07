4cc8662337e323a6785c86be7882059398d7c571
Nick <git@mailship.org>
2023-01-07 15:30:47 -0500

    cathedral-scraper.py: Update for new api (none).

--- cathedral-scraper.py
+++ cathedral-scraper.py
@@ -6,17 +6,33 @@ import subprocess
 import sys
 import tempfile
 import typing
+import bs4
 import requests
 import yaml
 
+# GET https://cathedral.org/calendar/?filters[modality]=in-person&filters[date]=&filters[types][0]=sightseeing-tours&query=&current_page=2
+
 DOMAIN = 'cathedral.org'
-CALENDAR_PATH = '/wp-admin/admin-ajax.php'
-USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0'
+CALENDAR_PATH = '/calendar/'
+USER_AGENT = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0.1) Gecko/20100101 Firefox/108.0.1'
 ZENITY_CMD = [
   'zenity', '--list', '--title', 'National Cathedral Events', '--width', '750', '--height', '300',
   '--print-column', '4', '--editable', '--column', 'Date', '--column', 'Time', '--column', 'Event',
   '--column', 'URL',
 ]
+ZENITY_ERROR_CMD = [
+  'zenity', '--error', '--no-markup', '--width', '400', '--title', 'Cathedral Scraper error',
+  '--text'
+]
+HTML_FIELDS = {
+  'date': {'tag':'span', 'class':'event_list_row_time_date', 'critical':True},
+  'day': {'tag':'span', 'class':'event_list_row_time_day'},
+  'title': {'tag':'span', 'class':'event_list_item_title_link_label', 'critical':True},
+  'url': {'tag':'a', 'class':'event_list_item_title_link', 'attr':'href'},
+  'time': {'tag':'span', 'class':'event_list_item_time_label_date_item'},
+  'location': {'tag':'span', 'class':'event_list_item_detail_label'},
+  'tickets': {'tag':'span', 'class':'event_list_item_tickets_link_label'}
+}
 SCRIPT_DIR = pathlib.Path(__file__).resolve().parent
 DEFAULT_FILTER_FILE = SCRIPT_DIR / 'cathedral-filters.yml'
 SILENCE_FILE = pathlib.Path('~/.local/share/nbsdata/SILENCE').expanduser()
@@ -27,7 +43,7 @@ tower climb."""
 def make_argparser():
   parser = argparse.ArgumentParser(add_help=False, description=DESCRIPTION)
   options = parser.add_argument_group('Options')
-  options.add_argument('pages', type=int, nargs='?', default=5,
+  options.add_argument('pages', type=int, nargs='?', default=8,
     help='How many pages of events to request. 5 is usually enough to get about a month of '
       'results. Default: %(default)s')
   options.add_argument('-f', '--filters', type=pathlib.Path, default=DEFAULT_FILTER_FILE,
@@ -62,13 +78,26 @@ def main(argv):
     return 1
 
   headers = {'user-agent':USER_AGENT}
+  try:
+    get_and_display_events(args.pages, headers, ignore, seen)
+  except RuntimeError as error:
+    cmd = ZENITY_ERROR_CMD + [str(error)]
+    subprocess.run(cmd, check=True)
 
+
+def get_and_display_events(pages, headers, ignore, seen):
   with tempfile.NamedTemporaryFile(mode='w+t', prefix='cathedral.', suffix='.txt') as tmpfile:
     results = 0
-    for event in get_events(f'https://{DOMAIN}{CALENDAR_PATH}', headers, args.pages):
-      if event.title not in ignore and (event.title, event.date) not in seen:
-        results += 1
-        print(event.date, event.time, event.title, event.url, sep='\n', file=tmpfile)
+    for event in get_events(f'https://{DOMAIN}{CALENDAR_PATH}', headers, pages):
+      logging.info(f'Found event on {event.date}: {event.title}')
+      if event.title in ignore:
+        logging.info('  in ignore list')
+        continue
+      if (event.title, event.date) in seen:
+        logging.info('  in seen list')
+        continue
+      results += 1
+      print(event.date, event.time, event.title, event.url, sep='\n', file=tmpfile)
     # If we found results, display them in a dialog.
     if results > 0:
       tmpfile.seek(0)
@@ -77,35 +106,57 @@ def main(argv):
 
 class Event(typing.NamedTuple):
   date: str
+  day: str
   title: str
   url: str
   time: str
   location: str
   tickets: str
-  mapping = {
-    'title':'event_title', 'url':'event_url', 'time':'event_time', 'location':'event_location',
-    'tickets':'tickets_url'
-  }
 
 
 def get_events(url, headers, pages):
   for page in range(1, pages+1):
-    data = {
-      'action': 'e11_search_calendar_dates',
-      'data': f'calendar_list_date=&calendar_empty_list_date=&paged={page}',
-      'paged': str(page)
+    params = {
+      'filters[modality]': 'in-person',
+      'filters[date]': '',
+      'filters[types][0]': 'sightseeing-tours',
+      'query': '',
+      'current_page': str(page)
     }
-    response = requests.post(url, headers=headers, data=data)
+    logging.info(f'Requesting page {page}')
+    response = requests.post(url, headers=headers, params=params)
     if response.status_code != 200:
-      fail(f'Received response code {response.status_code} {response.reason} on page {page}')
-    data = response.json()
-    for day in data['results']:
-      date = day['date_header']
-      for event in day['events']:
-        data = {'date':date}
-        for attr, field in Event.mapping.items():
-          data[attr] = event.get(field)
+      raise RuntimeError(f'Received response code {response.status_code} {response.reason} on page {page}')
+    html_bytes = response.content
+    soup = bs4.BeautifulSoup(html_bytes, 'html.parser')
+    for day_elem in soup.find_all('div', class_='event_list_row'):
+      date = find_child_text(day_elem, HTML_FIELDS['date'])
+      day = find_child_text(day_elem, HTML_FIELDS['day'])
+      for event_elem in day_elem.find_all('li', class_='event_list_item'):
+        data = {'date':date, 'day':day}
+        for field in Event._fields:
+          if field not in data:
+            data[field] = find_child_text(event_elem, HTML_FIELDS[field])
         yield Event(**data)
+
+
+def find_child_text(parent, spec):
+  child = parent.find(spec['tag'], class_=spec['class'])
+  if spec.get('attr'):
+    raw_text = child.attrs[spec['attr']]
+  else:
+    raw_text = child.text
+  if raw_text is None:
+    parent_desc = parent.name
+    class_ = parent.attrs.get("class")
+    if class_:
+      parent_desc += '.'+class_
+    message = f'Problem searching for child of {parent_desc} with spec {spec}'
+    if spec.get('critical'):
+      raise RuntimeError(message)
+    else:
+      logging.warning(message)
+  return raw_text.strip()
 
 
 def fail(message):
